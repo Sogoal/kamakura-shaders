3bd1c9d805023e9aa14a8bf3856864e1f3fc4063
单鑫 <xin.shan@outlook.com>
2018-12-10 14:39:27 +0800

    Rotate

---
+++ Assets/RotateHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateHandler : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.RotateAround(new Vector3(0f, 0.25f, 0),new Vector3(0,1,0), 1f);
+    }
+}
